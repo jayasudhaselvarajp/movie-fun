ef9c2e0e482f91954d250e8e3966f5231dc2ab6b
jayasudhaselvarajp <jayasudha.selvaraj@perficient.com>
2018-12-13 11:30:00 +0530

    Removing reads

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -6,15 +6,19 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class CsvUtils {
+    private static ClassLoader classLoader = CsvUtils.class.getClassLoader();
 
     public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+//        try {
+       // Class
+       // InputStream inputStream = classLoader.getResourceAsStream(path);
+            Scanner scanner = new Scanner(classLoader.getResourceAsStream(path)).useDelimiter("\\A");
 
             if (scanner.hasNext()) {
                 return scanner.next();
@@ -22,9 +26,9 @@ public class CsvUtils {
                 return "";
             }
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+//        } catch (FileNotFoundException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
